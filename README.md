6032188ccc0f1b76323a3b7c5dceaa92b49cb0e5
Cintia Capece <ccapece@oficinacys.com.ar>
2021-11-11 17:14:30 -0300

    Agrega pagina responsive de SuperMArioBross

---
+++ README.md
@@ -0,0 +1,5 @@
+# Curso de Udemy sobre TailwindCSS
+
+- Link:
+
+https://www.udemy.com/course/curso-express-de-tailwindcss-de-cero-a-experto/learn/lecture/27069354#overview
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,20 @@ module.exports = {
         'yellow-dots': "url('../img/pattern-yellow-dots.png')",
       }),
       screens:{
+        'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+          'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+        '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
         'xsm': '320px'
 
       }
